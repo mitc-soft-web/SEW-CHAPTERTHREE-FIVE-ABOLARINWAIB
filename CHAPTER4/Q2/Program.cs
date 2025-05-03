be5783c958a6ffcfa06d6142b8a90cd05207e480
ibromass <abolarinwaibraheem@gmail.com>
2025-05-03 12:05:13 +0100

    commit

--- CHAPTER4/Q2/Program.cs
+++ CHAPTER4/Q2/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter Radius :");
+int r = Convert.ToInt32(Console.ReadLine());
+double areaOfRectangle = 3.14 * r * r;
+Console.WriteLine(areaOfRectangle);
 
+double perimeterOfRectangle = 2 * 3.14 * r;
+Console.WriteLine(perimeterOfRectangle);
