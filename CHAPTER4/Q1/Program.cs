adb783fd2ac199ba37403fd1ddf2e1ad4d244eda
ibromass <abolarinwaibraheem@gmail.com>
2025-05-03 10:50:06 +0100

    commit

---
+++ CHAPTER4/Q1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter fisrtDigit:");
+int fisrtDigit = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter secondDigit:");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter thirdDigit:");
+int thirdDigit = Convert.ToInt32(Console.ReadLine());
+
+int sum = fisrtDigit + secondDigit + thirdDigit;
+Console.WriteLine(sum);
