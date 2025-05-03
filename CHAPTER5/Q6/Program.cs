0c5194b57057b5166610c8df2f06210782aa2676
ibromass <abolarinwaibraheem@gmail.com>
2025-05-03 05:10:28 +0100

    commit

--- CHAPTER5/Q6/Program.cs
+++ CHAPTER5/Q6/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+ Console.Write("Enter first number: ");
+    int a = Int32.Parse(Console.ReadLine());
+    Console.Write("Enter second number: ");
+    int b = Int32.Parse(Console.ReadLine());    
 
+    if (a > b)
+    {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    }
+    
+    Console.WriteLine("First number - {0}, Second number - {1}.", a, b);
--- CHAPTER5/Q7/Program.cs
+++ CHAPTER5/Q7/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter firstNum:");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter secondNum:");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter thirdNum:");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter forthNum:");
+int forthNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter fifthNum:");
+int fifthNum = Convert.ToInt32(Console.ReadLine());
+
+  if (firstNum < secondNum){
+       firstNum = secondNum;
+  } 
+    if (firstNum < thirdNum){
+       firstNum = thirdNum;
+    } 
+    if (firstNum < forthNum){
+      firstNum = forthNum;
+    } 
+    if (firstNum < fifthNum){
+      firstNum = fifthNum;
+    } 
+
+    Console.WriteLine($" The biggest number is {firstNum}");
 
